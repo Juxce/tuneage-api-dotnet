4f55f9c06315b0794edcef2eae39a6e6827f92b1
Al Rothschild <al@juxce.com>
2018-12-29 17:59:34 -0800

    This commit includes the first unit test added to the solution, using xUnit. The approach demonstrates some fundamentals about working with tests in EF Core and AspNetCore, which will be helpful to observe when doing integration tests as well. Moq is being used in the unit test to mock out the data context, providing it test data that the controller's logic can call against.
    A class called TestAsyncQueryProvider has been added as well. This is a very
    important class that serves as a test double of the provider that will process
    asynchronous queries during the unit tests. It is decorated with some comments
    and source URLs for further details. For API controllers, we may need to
    create a similar object, though I'm hoping this will work for those as well.
    
    For now, just a single test is listed, so that we can look at it together and
    discuss. There's a single line in the Assert portion of the test that is
    commented out, as it represents a change from EF6 and its syntax is troubling
    me. I would like to be able to verify that the method
    Labels.FirstOrDefaultAsync was called on the mocked data context. Obviously,
    it is getting called, but a change in syntax from the old FindAsync(int) to
    this new one, which expects an expression makes it difficult to understand how
    to tell Moq to verify it. Any help there would be great!

---
+++ Tuneage.WebApi.Tests.Unit/Controllers/Mvc/LabelsControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Tuneage.Data.Orm.EF.DataContexts;
+using Tuneage.Domain.Entities;
+using Tuneage.WebApi.Controllers.Mvc;
+using Xunit;
+
+namespace Tuneage.WebApi.Tests.Unit.Controllers.Mvc
+{
+    public class LabelsControllerTests : IDisposable
+    {
+        private Mock<DbSet<Label>> _mockLabelSet;
+        private Mock<TuneageDataContext> _mockContext;
+        private LabelsController _controller;
+
+        public LabelsControllerTests()
+        {
+            _mockLabelSet = new Mock<DbSet<Label>>();
+            _mockContext = new Mock<TuneageDataContext>(new DbContextOptions<TuneageDataContext>());
+        }
+
+        [Fact]
+        public async Task DetailsGet_ShouldGetDetailsForExistingAlbum()
+        {
+            // Arrange
+            var existingLabel = new Label() { LabelId = 9, Name = "Fat Wreck Chords", WebsiteUrl = "www.fatwreck.com" };
+            var data = new List<Label> { existingLabel }.AsQueryable();
+            _mockLabelSet.As<IAsyncEnumerable<Label>>().Setup(mls => mls.GetEnumerator()).Returns(
+                    new TestAsyncEnumerator<Label>(data.GetEnumerator())
+                );
+            _mockLabelSet.As<IQueryable<Label>>().Setup(mls => mls.Provider).Returns(
+                    new TestAsyncQueryProvider<Label>(data.Provider)
+                );
+            _mockLabelSet.As<IQueryable<Label>>().Setup(mls => mls.Expression).Returns(data.Expression);
+            _mockLabelSet.As<IQueryable<Label>>().Setup(mls => mls.ElementType).Returns(data.ElementType);
+            _mockLabelSet.As<IQueryable<Label>>().Setup(mls => mls.GetEnumerator()).Returns(() => data.GetEnumerator());
+            _mockContext.Setup(mc => mc.Labels).Returns(_mockLabelSet.Object);
+            _controller = new LabelsController(_mockContext.Object);
+
+            // Act
+            var result = await _controller.Details(existingLabel.LabelId);
+            var viewResult = (ViewResult)result;
+            var model = (Label)viewResult.Model;
+
+            // Assert
+            //_mockContext.Verify(c => c.Labels.FirstOrDefaultAsync(9), times: Times.Once);  // HOW TO verify this now? Is it even necessary?
+            Assert.Null(viewResult.ViewName);
+            Assert.Equal(existingLabel, model);
+        }
+
+
+
+
+
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Tuneage.WebApi.Tests.Unit/TestAsyncQueryProvider.cs
@@ -0,0 +1,108 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// This object was sourced from a Stack Overflow question/answer, which itself references the same MSDN article
+// ("Entity Framework Testing with a Mocking Framework (EF6 onwards)") that I used in EF6 to build an asynchronous
+// test provider for unit testing of Controllers. This new version represents some syntactical changes from the EF6
+// world - most prominently, inheriting from IAsyncQueryProvider, whereas before, it was called IDbAsyncQueryProvider. 
+// 
+// https://stackoverflow.com/questions/40476233/how-to-mock-an-async-repository-with-entity-framework-core
+// https://msdn.microsoft.com/en-us/data/dn314429.aspx
+//
+// The purpose of this object is to serve as a test double of the the provider that will process asynchronous queries
+// during unit tests that are using mocked DbSet and DbContext objects.
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace Tuneage.WebApi.Tests.Unit
+{
+    internal class TestAsyncQueryProvider<TEntity> : IAsyncQueryProvider
+    {
+        private readonly IQueryProvider _inner;
+
+        internal TestAsyncQueryProvider(IQueryProvider inner)
+        {
+            _inner = inner;
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            return new TestAsyncEnumerable<TEntity>(expression);
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            return new TestAsyncEnumerable<TElement>(expression);
+        }
+
+        public object Execute(Expression expression)
+        {
+            return _inner.Execute(expression);
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            return _inner.Execute<TResult>(expression);
+        }
+
+        public IAsyncEnumerable<TResult> ExecuteAsync<TResult>(Expression expression)
+        {
+            return new TestAsyncEnumerable<TResult>(expression);
+        }
+
+        public Task<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute<TResult>(expression));
+        }
+    }
+
+    internal class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+    {
+        public TestAsyncEnumerable(IEnumerable<T> enumerable)
+            : base(enumerable)
+        { }
+
+        public TestAsyncEnumerable(Expression expression)
+            : base(expression)
+        { }
+
+        public IAsyncEnumerator<T> GetEnumerator()
+        {
+            return new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+
+        IQueryProvider IQueryable.Provider
+        {
+            get { return new TestAsyncQueryProvider<T>(this); }
+        }
+    }
+
+    internal class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _inner;
+
+        public TestAsyncEnumerator(IEnumerator<T> inner)
+        {
+            _inner = inner;
+        }
+
+        public void Dispose()
+        {
+            _inner.Dispose();
+        }
+
+        public T Current
+        {
+            get { return _inner.Current; }
+        }
+
+        public Task<bool> MoveNext(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_inner.MoveNext());
+        }
+    }
+}
