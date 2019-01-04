73e54f37123f1ac2230eea29e034466f70a827fa
John Michael Miller <jMM@juxce.com>
2019-01-04 13:57:56 -0800

    Add Test Data

--- Tuneage.Data/TestData/TestDataGraph.cs
+++ Tuneage.Data/TestData/TestDataGraph.cs
@@ -9,12 +9,13 @@ namespace Tuneage.Data.TestData
         {
             new Label() { LabelId = 1, Name = "Mercury", WebsiteUrl = "www.islandrecords.com/labels/mercury" },
             new Label() { LabelId = 2, Name = "Atlantic", WebsiteUrl = "www.alanticrecords.com" },
-            new Label() { LabelId = 3, Name = "Capitol", WebsiteUrl = "www.capitolrecords.com" }
+            new Label() { LabelId = 3, Name = "Capitol", WebsiteUrl = "www.capitolrecords.com" },
+            new Label() { LabelId = 4, Name = "Fat Wreck Chords", WebsiteUrl = "www.fatwreck.com" }
         };
 
         public static List<Label> LabelsAlphabetizedByLabelName = new List<Label>()
         {
-            LabelsRaw[2], LabelsRaw[3], LabelsRaw[1]
+            LabelsRaw[2], LabelsRaw[3], LabelsRaw[4], LabelsRaw[1]
         };
     }
 }
