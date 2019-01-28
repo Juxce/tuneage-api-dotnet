98a9e6c69ca413fffba0e19e30483643e4006893
John Michael Miller <jMM@juxce.com>
2019-01-27 20:02:02 -0800

    add deployment scripts

---
+++ Tuneage.Deploy/ps/Create-KeyVault.ps1
@@ -0,0 +1,18 @@
+$keyVaultName = "TuneageSecrets"
+$resourceGroupName="TuneageWebAPI"
+$location='West US 2'
+$userPrincipalName='jMM@juxce.com'
+
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+
+New-AzureRmKeyVault `
+  -VaultName $keyVaultName `
+  -resourceGroupName $resourceGroupName `
+  -Location $location `
+  -EnabledForTemplateDeployment
+Set-AzureRmKeyVaultAccessPolicy -VaultName $keyVaultName -UserPrincipalName $userPrincipalName -PermissionsToSecrets set,delete,get,list
+
+$password = openssl rand -base64 32
+echo $password
+$secretvalue = ConvertTo-SecureString $password -AsPlainText -Force
+Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name "vmAdminPassword" -SecretValue $secretvalue
